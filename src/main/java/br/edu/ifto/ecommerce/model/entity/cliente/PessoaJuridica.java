6740f1deef9212d49239ee0e69cd0b444bac7e5a
Juno Lupus <junoplupus@gmail.com>
2026-05-05 16:15:50 -0300

    feat: Adicionado métodos de identificação e mascaramento de documento em Pessoa
    - Adicionado métodos abstratos getTipo, getDocumento e getDocumentoMascarado em Pessoa ;
    
    - Implementado métodos em PessoaFisica e PessoaJuridica.

--- src/main/java/br/edu/ifto/ecommerce/model/entity/cliente/PessoaJuridica.java
+++ src/main/java/br/edu/ifto/ecommerce/model/entity/cliente/PessoaJuridica.java
@@ -27,7 +27,22 @@ public class PessoaJuridica extends Pessoa {
     }
 
     @Override
+    public char getTipo() {
+        return 'J';
+    }
+
+    @Override
     public String getNomeExibicao() {
         return getRazaoSocial();
+    }
+
+    @Override
+    public String getDocumento() {
+        return getCnpj();
+    }
+
+    @Override
+    public String getDocumentoMascarado() {
+        return "**" + getDocumento().substring(2, 10) + "****-**";
     }
 }
