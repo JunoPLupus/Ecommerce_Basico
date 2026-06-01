71f7ce96ae9757e48a413a077f94d02852a4cd2f
Juno Lupus <junoplupus@gmail.com>
2026-06-01 18:13:04 -0300

    refactor: Extrai lógica de cadastro de cliente para ClienteService

--- src/main/java/br/edu/ifto/ecommerce/controller/cliente/ClienteController.java
+++ src/main/java/br/edu/ifto/ecommerce/controller/cliente/ClienteController.java
@@ -1,15 +1,9 @@
 package br.edu.ifto.ecommerce.controller.cliente;
 
-import br.edu.ifto.ecommerce.model.entity.cliente.Pessoa;
 import br.edu.ifto.ecommerce.model.entity.cliente.PessoaFisica;
 import br.edu.ifto.ecommerce.model.entity.cliente.PessoaJuridica;
-import br.edu.ifto.ecommerce.model.entity.role.Role;
-import br.edu.ifto.ecommerce.model.entity.usuario.Usuario;
-import br.edu.ifto.ecommerce.model.repository.ClienteRepository;
-import br.edu.ifto.ecommerce.model.repository.RoleRepository;
-import br.edu.ifto.ecommerce.model.repository.UsuarioRepository;
+import br.edu.ifto.ecommerce.service.ClienteService;
 import lombok.AllArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 
 import static br.edu.ifto.ecommerce.utils.Diretorios.HTML_CLIENTE_FORM;
-import static br.edu.ifto.ecommerce.utils.Roles.ROLE_USER;
 import static br.edu.ifto.ecommerce.utils.Rotas.*;
 
 @Controller
@@ -26,10 +19,7 @@ import static br.edu.ifto.ecommerce.utils.Rotas.*;
 @RequestMapping(CLIENTES)
 public class ClienteController {
 
-    private final RoleRepository roleRepository;
-    private final UsuarioRepository usuarioRepository;
-    private final ClienteRepository clienteRepository;
-    private final PasswordEncoder passwordEncoder;
+    private final ClienteService clienteService;
 
     @GetMapping(CADASTRO)
     public String insert(){
@@ -47,7 +37,7 @@ public class ClienteController {
             model.addAttribute("erro", mensagem);
             return HTML_CLIENTE_FORM;
         }
-        insertClienteUsuario(pessoa, login, password);
+        clienteService.insert(pessoa, login, password);
         return "redirect:/" + CADASTRO_CLIENTE;
     }
 
@@ -62,17 +52,7 @@ public class ClienteController {
             model.addAttribute("erro", mensagem);
             return HTML_CLIENTE_FORM;
         }
-        insertClienteUsuario(pessoa, login, password);
+        clienteService.insert(pessoa, login, password);
         return "redirect:/" + CADASTRO_CLIENTE;
-    }
-
-    private void insertClienteUsuario(Pessoa pessoa, String login, String password){
-        Pessoa pessoaSalva = clienteRepository.insert(pessoa);
-
-        Role roleUser = roleRepository.findByNome(ROLE_USER);
-        String senhaCriptografada = passwordEncoder.encode(password);
-        Usuario usuarioNovo = new Usuario(pessoaSalva, login, senhaCriptografada, roleUser);
-
-        usuarioRepository.insert(usuarioNovo);
     }
 }
