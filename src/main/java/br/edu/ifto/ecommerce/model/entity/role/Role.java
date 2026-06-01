ff9f52a017bed27862eb5f08cbfa42ea4f34cd90
Juno Lupus <junoplupus@gmail.com>
2026-06-01 16:53:26 -0300

    feat: Adiciona entidades Usuario e Role com relacionamento ManyToMany

---
+++ src/main/java/br/edu/ifto/ecommerce/model/entity/role/Role.java
@@ -0,0 +1,42 @@
+package br.edu.ifto.ecommerce.model.entity.role;
+
+import br.edu.ifto.ecommerce.model.entity.usuario.Usuario;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+public class Role implements Serializable, GrantedAuthority {
+    @Setter(AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "{erro.role.nome.obrigatorio}}}")
+    @NotBlank(message = "{erro.role.nome.obrigatorio}}")
+    @Pattern(regexp = "ROLE_ADMIN|ROLE_USER", message = "{erro.role.nome.pattern}")
+    private String nome;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<Usuario> usuarios;
+
+    public Role(){ this.usuarios = new ArrayList<>(); }
+
+    @Override
+    public String getAuthority() {
+        return nome;
+    }
+}
---
+++ src/main/java/br/edu/ifto/ecommerce/model/entity/usuario/Usuario.java
@@ -0,0 +1,64 @@
+package br.edu.ifto.ecommerce.model.entity.usuario;
+
+import br.edu.ifto.ecommerce.model.entity.cliente.Pessoa;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import br.edu.ifto.ecommerce.model.entity.role.Role;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Usuario implements Serializable, UserDetails {
+    @Setter(AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "{erro.usuario.pessoa.obrigatorio}")
+    @OneToOne
+    private Pessoa pessoa;
+
+    @NotNull(message = "{erro.usuario.login.obrigatorio}")
+    @NotBlank(message = "{erro.usuario.login.obrigatorio}")
+    @Size(min = 3, message = "{erro.usuario.login.tamanho.min}")
+    @Column(unique = true)
+    private String login;
+
+    @NotNull(message = "{erro.usuario.senha.obrigatorio}")
+    @NotBlank(message = "{erro.usuario.senha.obrigatorio}")
+    @Size(min = 6, message = "{erro.usuario.senha.tamanho.min}")
+    private String password;
+
+    @NotNull(message = "{erro.usuario.roles.obrigatorio}")
+    @Size(min = 1, max = 2, message = "{erro.usuario.roles.tamanho}")
+    @ManyToMany
+    private List<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+}
